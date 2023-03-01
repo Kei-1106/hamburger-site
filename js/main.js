918fb7f0a5a7d1d10a9dde2bb77216c12de000c1
Kei <keigyou.workword.1106@gmail.com>
2023-03-01 23:21:52 +0900

    スマホファーストに変更

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 $(function(){
-    $(".p-search-box-A").on("click", function(){
-        if()
+    //フォーカス時にアイコンを消す
+    $(".p-search-box").focus(function(){
+        $(this).addClass("p-search-box__icon-focus");
+    });
+    //フォーカスが外れた時、空ならばアイコンを表示
+    $(".p-search-box").blur(function(){
+        if($(this).val() == ""){
+            $(this).removeClass("p-search-box__icon-focus");
+        }
+    });
+});
+
+
+$(function(){
+const ham = $(".js-hamburger");
+const btn = $(".l-header__hamburger");
+const side = $(".l-sidebar__hamburger");
+const hide = $(".l-main-hide-contents");
+
+var durationSideMenu = 300;
+
+ham.on('click', function(){
+    side.toggleClass("l-sidebar__hamburger-active");
+    btn.toggleClass("l-header__hamburger-active");
+    hide.toggleClass("l-main-hide-contents-active");
     });
 });
